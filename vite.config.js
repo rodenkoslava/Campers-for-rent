87a0c89314ac465a253ec9b38e361c9acf3dc81f
rodenkoslava <131722989+rodenkoslava@users.noreply.github.com>
2024-03-23 09:01:39 +0100

    Deploying Vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
+  base: "/https://github.com/rodenkoslava/Campers-for-rent",
   plugins: [react(), svgr()],
   resolve: {
     alias: {
